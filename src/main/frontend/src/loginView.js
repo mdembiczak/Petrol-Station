a70839cd9e86e967bc1f71ecbad25601ee539132
Bigmilk120 <sprawdzany96@gmail.com>
2019-03-27 10:53:09 +0100

    Made: LoginView.js with simple form (login and password) loginView.css with simple css for form Changed in index.js to use LoginView.js instead of App.js

---
+++ src/main/frontend/src/loginView.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './loginView.css';
+
+class loginView extends Component {
+    constructor(props){
+        super(props);
+        this.state = {login: '',
+                        password: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleChangePassword = this.handleChangePassword.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.login.value + ", " + this.password.value);
+        event.preventDefault();
+    }
+    handleChange(event) {
+        this.setState({login: event.target.value});
+     }
+    handleChangePassword(event) {
+        this.setState({password: event.target.value});
+    }
+
+  render() {
+    return (
+      <div className="loginView">
+        <header className="loginView-header">
+            <form onSubmit={this.handleSubmit}>
+            <label>
+            <p>Podaj login:</p>
+            <input type="text" value={this.state.login} onChange={this.handleChange} /></label><br/>
+            <label>
+            <p>Podaj hasło:</p>
+            <input type="password" value={this.state.password} onChange={this.handleChangePassword} /></label><br/>
+            <input type="submit" value="Wyślij" />
+            </form>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default loginView;
