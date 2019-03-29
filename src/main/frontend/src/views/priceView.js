3a5e0de5375547ddcb6ffcda356ff021977bf2d3
Bigmilk120 <sprawdzany96@gmail.com>
2019-03-29 16:52:24 +0100

    Made: Trying to get pricelist with axios.

--- src/main/frontend/src/views/priceView.js
+++ src/main/frontend/src/views/priceView.js
@@ -2,48 +2,53 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import logo from '../logo.svg';
 import '../styles/priceView.css';
+import _ from 'lodash';
 
-class loginView extends Component {
-    constructor(props){
-        super(props);
-        this.values = {PB98: '4.61zł',
-                        PB95: '4.40zł',
-                        LPG: '1.99zł',
-                        ON: '5.20zł',
-                        Myjnia: '1.20zł'};
+class priceView extends Component {
+  constructor(props) {
+   super(props);
 
+   this.state = {
+     array: []
+   };
+
+   this.renderPrices = this.renderPrices.bind(this);
+ }
+
+ componentDidMount(){
+   axios
+     .get('localhost:8080/prices')
+     .then(({ data })=> {
+       console.log(data);
+       this.setState(
+         { array: data.prices }
+       );
+     })
+     .catch((err)=> {})
+ }
+
+ render() {
+   console.log(this.state.array);
+   return(
+     <div>
+       <h3>Prices</h3>
+       <ul className="list-group">
+          {this.renderPrices()}
+       </ul>
+     </div>
+   );
+ }
+
+ renderPrices() {
+   console.log(this.state.array);
+   return _.map(this.state.array, price => {
+     return (
+       <li className="list-group-item" key={price.fuelType}>
+           {price.fuelType}
+       </li>
+     );
+   });
+ }
 }
-state = {
-    values: []
-}
-componentDidMount(){
-    axios.get('https://localhost:8080/prices')
-        .then(res => {
-            const values = res.data;
-            this.setState({values});
-        })
-}
 
-  render() {
-    return (
-      <div className="priceView">
-        <header className="priceView-header">
-                    { this.state.values.map(values=> <p>{values.fuelType}</p>)}
-            <table>
-                <tr><td className="table-td1">Usługa</td><td className="table-td1">Cena</td></tr>
-                <tr><td className="table-td11">PB95</td><td>{this.values.PB95}</td></tr>
-                <tr><td className="table-td11">PB98</td><td>{this.values.PB98}</td></tr>
-                <tr><td className="table-td11">LPG</td><td>{this.values.LPG}</td></tr>
-                <tr><td className="table-td11">ON</td><td>{this.values.ON}</td></tr>
-                <tr><td className="table-td11">Myjnia</td><td>{this.values.Myjnia}</td></tr>
-            </table>
-
-
-
-        </header>
-      </div>
-    );
-  }
-}
-
-export default loginView;
+export default priceView;
