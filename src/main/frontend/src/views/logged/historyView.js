3afc84e821da1f359f16de6e3d713ad7b9b0abe5
Bigmilk120 <sprawdzany96@gmail.com>
2019-04-02 15:57:29 +0200

    Made: historyView with no styles.

---
+++ src/main/frontend/src/views/logged/historyView.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import "../../styles/logged/historyView.css";
+
+class historyView extends Component {
+  constructor() {
+    super();
+    this.state = {
+      prices: []
+    };
+  }
+
+
+
+  componentDidMount() {
+    fetch('http://localhost:8080/service?callback=foo')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      this.setState({ prices: data});
+    })
+    .catch(error => console.log(error));
+  }
+
+  render() {
+    const { prices } = this.state;
+    return (
+      <div className="historyView">
+        <h3>Historia</h3>
+            <table>
+            <tr><td className="historyView_c11">Usługa</td><td className="historyView_c12">Cena</td></tr>
+           {prices.map(function(item, key) {
+
+               return (
+
+                  <tr key = {key}>
+                      <td>{item.serviceType}</td><td>{item.price}</td>
+                  </tr>
+
+                )
+
+            })}
+            </table>
+      </div>
+    );
+  }
+}
+
+export default historyView;
--- src/main/java/com/petrolstation/controllers/services/ServiceController.java
+++ src/main/java/com/petrolstation/controllers/services/ServiceController.java
@@ -1,9 +1,29 @@
 package com.petrolstation.controllers.services;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.petrolstation.models.services.Reservation;
+import com.petrolstation.models.services.Service;
+import com.petrolstation.repositories.services.ReservationRepository;
+import com.petrolstation.repositories.services.ServiceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/service")
 public class ServiceController {
+
+    @Autowired
+    ServiceRepository serviceRepository;
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Service> showServices(){
+        return serviceRepository.findAll();
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public void addPrice(@RequestBody Service service){
+        serviceRepository.save(service);
+    }
 }
