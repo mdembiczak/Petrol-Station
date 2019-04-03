2b3de49948dfc90723a6a05bf0b34e382b3f3d3b
Damian Czyż <vrigos90@gmail.com>
2019-04-03 19:11:34 +0200

    It's alive! Working adding new user to DB

--- src/main/frontend/src/views/registerView.js
+++ src/main/frontend/src/views/registerView.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../styles/registerView.css';
+import fetch from 'isomorphic-fetch';
+import axios from 'axios';
+
 
 class registerView extends Component {
     constructor(props){
@@ -18,18 +21,45 @@ class registerView extends Component {
         this.handleChangePassword = this.handleChangePassword.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
+
     handleSubmit(event) {
-             alert('A name was submitted: ' + this.state.email + ", " + this.state.password);
-             event.preventDefault();
-         }
-         handleChange(event) {
-             this.setState({[event.target.name] : event.target.value});
-          }
-         handleChangePassword(event) {
-             this.setState({[event.target.name] : event.target.value});
-         }
+        event.preventDefault();
+
+        var data = {
+            firstName: this.state.firstName,
+            lastName: this.state.lastName,
+            city: this.state.city,
+            postalcode: this.state.postalcode,
+            email: this.state.email,
+            password: this.state.password
+        }
+
+        fetch('http://localhost:8080/users', {
+          method: 'POST',
+          headers: {
+                      'Accept': 'application/json',
+                      'Content-Type': 'application/json'
+                  },
+          body: JSON.stringify(data)
+        }).then(obj => console.log(obj))
+        .then(response => console.log('Success:', JSON.stringify(response)))
+        .catch(error => console.error('Error:', error));
+
+
+    }
+
+
+
+    handleChange(event) {
+       this.setState({[event.target.name] : event.target.value});
+    }
+
+    handleChangePassword(event) {
+       this.setState({[event.target.name] : event.target.value});
+    }
 
   render() {
+  const {firstName, lastName, city, postalcode, email, password, confirmPassword} = this.state;
     return (
       <div className="registerView">
         <div className="registerView-header">
@@ -38,28 +68,28 @@ class registerView extends Component {
             <div className="leftSide">
               <label>
                     <p>Podaj adres e-mail:</p>
-                    <input type="email" name="email" value={this.state.email} onChange={this.handleChange} /></label><br/>
+                    <input type="email" name="email" value={email} onChange={this.handleChange} required /></label><br/>
                 <label>
                     <p>Podaj hasło:</p>
-                    <input type="password" name="password" value={this.state.password} onChange={this.handleChangePassword} /></label><br/>
+                    <input type="password" name="password" value={password} onChange={this.handleChangePassword} required/></label><br/>
                 <label>
                     <p>Potwierdź hasło:</p>
-                    <input type="password"  name="confirmPassword" value={this.state.confirmPassword} onChange={this.handleChangePassword} /></label><br/>
+                    <input type="password"  name="confirmPassword" value={confirmPassword} onChange={this.handleChangePassword} required/></label><br/>
               </div>
 
             <div className="rightSide">
                 <label>
-                    <p>Podaj imię:</p>
-                    <input type="text" name="firstName" value={this.state.firstName} onChange={this.handleChange} /></label><br/>
+                    <p>Podaj imię: </p>
+                    <input type="text" name="firstName" value={firstName} onChange={this.handleChange} required/></label><br/>
                 <label>
                     <p>Podaj nazwisko:</p>
-                    <input type="text" name="lastName" value={this.state.lastName} onChange={this.handleChange} /></label><br/>
+                    <input type="text" name="lastName" value={lastName} onChange={this.handleChange} required/></label><br/>
                 <label>
                     <p>Podaj miasto:</p>
-                    <input type="text" name="city" value={this.state.city} onChange={this.handleChange} /></label><br/>
+                    <input type="text" name="city" value={city} onChange={this.handleChange} required/></label><br/>
                 <label>
                     <p>Podaj kod pocztowy</p>
-                    <input type="text"  name="postalcode" value={this.state.postalcode} onChange={this.handleChange} /></label><br/>
+                    <input type="text"  name="postalcode" value={postalcode} onChange={this.handleChange} required/></label><br/>
             </div>
 
             <div className="submit">
