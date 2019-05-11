c96d970d59e6217dfafb49dd3716be3d6250abec
Bigmilk120 <sprawdzany96@gmail.com>
2019-05-11 12:45:29 +0200

    Nie działa

--- src/main/java/com/petrolstation/controllers/services/ReservationController.java
+++ src/main/java/com/petrolstation/controllers/services/ReservationController.java
@@ -1,9 +1,30 @@
 package com.petrolstation.controllers.services;
 
+import com.petrolstation.models.services.Reservation;
+import com.petrolstation.repositories.services.ReservationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/reservation")
 public class ReservationController {
+
+    @Autowired
+    ReservationRepository reservationRepository;
+
+    @RequestMapping(method = RequestMethod.GET, value="/getReservations")
+    public List<Reservation> getReservations(@RequestBody String id){
+        return reservationRepository.findAllByUserId(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value="/setReservation")
+    public void setReservation(@RequestBody Reservation res){
+        reservationRepository.save(res);
+    }
+
 }
