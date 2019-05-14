8b857e8ea6bead2334341db1115bc6bc588a8c56
Bigmilk120 <sprawdzany96@gmail.com>
2019-05-14 17:49:47 +0200

    Invoices to do.

--- src/main/frontend/src/views/worker/invoiceView.js
+++ src/main/frontend/src/views/worker/invoiceView.js
@@ -2,45 +2,79 @@ import React, { Component } from "react";
 import "../../styles/worker/reportsView.css";
 
 class invoiceView extends Component {
+    constructor() {
+        super();
+        this.state = {
+            services: [{name: "", price: ""}],
+            Nazwa: "",
+            NIP: "",
+            Data: "",
+
+        }
+    }
+    handleChange = (e) => {
+        if (["name", "price"].includes(e.target.className) ) {
+            let services = [...this.state.services]
+            services[e.target.dataset.id][e.target.className] = e.target.value.toUpperCase()
+            this.setState({ cats: services }, () => console.log(this.state.services))
+        } else {
+            this.setState({ [e.target.name]: e.target.value})
+        }
+    }
+    addService = (e) => {
+        this.setState((prevState) => ({
+            services: [...prevState.services, {name:"", price:""}],
+        }));
+    }
+    handleSubmit = (e) => { e.preventDefault() }
+
+
+    handleThing(){
+        console.log(this.state.services);
+    }
+
 
     render() {
+        let {Nazwa, NIP, Data, services} = this.state
         return (
-            <div>
-                <h2>Faktury</h2>
-                <table className="table">
-                    <thead className="thead-dark">
-                    <tr>
-                        <th scope="col">Lp.</th>
-                        <th scope="col">Data</th>
-                        <th scope="col">Kwota</th>
-                        <th scope="col">Klient</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>01.01.2019</td>
-                        <td>122,21zł</td>
-                        <td>Mariusz Kałamarz</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>02.01.2019</td>
-                        <td>50,01zł</td>
-                        <td>Kazimierz Wójcik</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>06.01.2019</td>
-                        <td>320,20zł</td>
-                        <td>Sylwester Stallone</td>
-                    </tr>
-                    </tbody>
-                </table>
-                <button>Nowa faktura</button>
-            </div>
-        );
+            <form onSubmit={this.handleSubmit} onChange={this.handleChange} >
+                <label htmlFor="Nazwa">Nazwa</label>
+                <input type="text" name="Nazwa" id="Nazwa" value={Nazwa} /><br/>
+                <label htmlFor="NIP">NIP</label>
+                <input type="text" name="NIP" id="NIP" value={NIP} /><br/>
+                <label htmlFor="Data">Data</label>
+                <input type="date" name="Data" id="Data" value={Data} /><br/>
+                <button onClick={this.addService}>Add new service</button>
+                {
+                    services.map((val, idx)=> {
+                        let serviceId = `name-${idx}`, priceId = `price-${idx}`
+                        return (
+                            <div key={idx}>
+                                <label htmlFor={serviceId}>{`Usługa #${idx + 1}`}</label>
+                                <input
+                                    type="text"
+                                    name={serviceId}
+                                    data-id={idx}
+                                    id={serviceId}
+                                    value={services[idx].name}
+                                    className="name"
+                                />
+                                <label htmlFor={priceId}>Age</label>
+                                <input
+                                    type="text"
+                                    name={priceId}
+                                    data-id={idx}
+                                    id={priceId}
+                                    value={services[idx].price}
+                                    className="price"
+                                />
+                            </div>
+                        )
+                    })
+                }
+                <input type="submit" value="Submit" />
+            </form>
+        )
     }
 }
-
 export default invoiceView;
